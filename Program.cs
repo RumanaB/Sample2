fea9a673a653a464cddf93cfdc082d1a0c35f2ab
RumanaB <rumana.badar@gmail.com>
2018-12-13 11:12:18 +0530

    aa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hey there Hexagon!!  This is a new line
+            Console.WriteLine("Hello Github!");
+            Student student = new Student();
+            student.StudentId = 1;
+            student.StudentName = "Hexagon";
+            student.Question = "Can Captain America save IronMan?"; //this line has been modified
+            Console.WriteLine($"Student {student.StudentName} has a question");
+            Console.WriteLine(student.Question);
+            
+            //the line above was removed
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Question { get; set; }
+
+    }
+}
