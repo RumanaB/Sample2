cf3ebf61aee58b4a73fc733f61296ff21bf98b90
RumanaB <rumana.badar@gmail.com>
2018-12-13 11:32:26 +0530

    login func added

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,13 +6,23 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Github!");
-            Student student = new Student();
-            student.StudentId = 1;
-            student.StudentName = "Hexagon";
-            student.Question = "Can Elon Musk save IronMan?";
-            Console.WriteLine($"Student {student.StudentName} has a question");
-            Console.WriteLine(student.Question);
+            //Added a login functionality
+            Console.WriteLine("Enter your username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+            if (username == "Rumzi" && password == "Rumzi")
+            {
+                Console.WriteLine("Hello Github!");
+                Student student = new Student();
+                student.StudentId = 1;
+                student.StudentName = "Hexagon";
+                student.Question = "Can Elon Musk save IronMan?";
+                Console.WriteLine($"Student {student.StudentName} has a question");
+                Console.WriteLine(student.Question);
+            }
+            else
+                Console.WriteLine("Ahoy Imposter!");
             Console.ReadLine();
         }
     }
