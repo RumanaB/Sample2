632fdc28338436fff6b453ec94b73f8286d6c1a4
RumanaB <rumana.badar@gmail.com>
2018-12-13 11:03:15 +0530

    Initial Commit
    Student Class added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Github!");
+            Student student = new Student();
+            student.StudentId = 1;
+            student.StudentName = "Hexagon";
+            student.Question = "Can Nasa save IronMan?";
+            Console.WriteLine($"Student {student.StudentName} has a question");
+            Console.WriteLine(student.Question);
+            Console.ReadLine();
+        }
+    }
+}
